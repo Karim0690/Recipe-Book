50df85af740ae566704391f47e537cb8f6570fb5
Karim Abdelkareem <132012097+Karim0690@users.noreply.github.com>
2024-08-27 12:28:50 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ShoppingListComponent } from './Components/shopping-list/shopping-list.component';
+import { RecipesComponent } from './Components/recipes/recipes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    title: 'Recipes',
+  },
+  {
+    path: 'shopping',
+    component: ShoppingListComponent,
+    title: 'Shopping',
+  },
+];
