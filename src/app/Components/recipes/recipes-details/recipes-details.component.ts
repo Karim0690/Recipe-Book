a856f4a55b3bcad3953f820bf23856f79df27e1d
Karim Abdelkareem <132012097+Karim0690@users.noreply.github.com>
2024-08-28 11:23:27 +0300

    add recipes

--- src/app/Components/recipes/recipes-details/recipes-details.component.ts
+++ src/app/Components/recipes/recipes-details/recipes-details.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../../../Models/recipes';
 
 @Component({
   selector: 'app-recipes-details',
   standalone: true,
   imports: [],
   templateUrl: './recipes-details.component.html',
-  styleUrl: './recipes-details.component.scss'
+  styleUrl: './recipes-details.component.scss',
 })
 export class RecipesDetailsComponent {
-
+  recipe: Recipes[] = [
+    new Recipes(
+      'Mash Potatto',
+      'New Discription of this recipes',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI9aAd5BIxeGUIprcMW_0jekZFxJGdB1EmVA&s'
+    ),
+    new Recipes(
+      'Scrscrambled eggs',
+      'New Discription of this recipes',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr7QrKLJJUb_YoXCoukPWANJugYErndlUI-Q&s'
+    ),
+  ];
 }
